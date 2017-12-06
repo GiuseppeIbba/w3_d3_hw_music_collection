3cc958d0838af2c4b746fb139c1add9ffc1f4e72
Richard Allison <richard.allison.18@outlook.com>
2017-12-06 15:44:06 +0000

    initial commit

---
+++ db/music_collection.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE album (
+  id SERIAL4 PRIMARY KEY,
+  artist_id INT8 REFERENCES artists(id),
+  title VARCHAR(255),
+  genre VARCHAR(255)
+);
